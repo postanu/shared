cafbf5794bc3d2e1fa499306ff7e452785ee702f
Eduard Aksamitov <e@edaks.co>
2021-09-19 22:09:30 +0300

    feat(types): `Post`

--- types/index.d.ts
+++ types/index.d.ts
@@ -53,3 +53,24 @@ export type Attachment = {
 	type: AttachmentType
 	url: string
 }
+
+export type PostState =
+	| 0
+	| 100
+	| 200
+
+export type PostVariant = {
+	text: string
+	pagesIds: string[]
+	attachmentsIds: string[]
+}
+
+export type Post = {
+	id: string
+	authorId: string
+	projectId: string
+	time: number
+	authorsIds: string[]
+	variants: PostVariant[]
+	state: PostState
+}
