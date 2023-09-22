f7dc8f5745600eb7895b16f3f8b5f9faa9726069
Eduard Aksamitov <e@euaaaio.ru>
2023-09-22 13:33:52 +0300

    feat: `post-activity`

---
+++ post-activity/index.d.ts
@@ -0,0 +1,11 @@
+export type PostsActivity = { id: string } & PostsActivityFields
+
+export type PostsActivityType = 'action' | 'message'
+
+export type PostsActivityFields = {
+	postId: string
+	type: PostsActivityType
+	message: string
+	args: string
+	time: string
+}
