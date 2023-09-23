79733c6118b6e7d2e0b1a593ac983ed8ba4103e3
Eduard Aksamitov <e@euaaaio.ru>
2023-09-23 22:37:42 +0300

    feat: better `PostsActivity`

--- post-activity/index.d.ts
+++ post-activity/index.d.ts
@@ -1,10 +1,29 @@
 export type PostsActivity = { id: string } & PostsActivityFields
 
-export type PostsActivityType = 'action' | 'message'
+export type PostsActivityActionMessage =
+	| 'post/created'
+	| 'post/edited/date'
+	| 'post/edited/time'
+	| 'post/edited/variant/pages'
+	| 'post/edited/variant/text'
+	| 'post/review/accepted'
+	| 'post/review/rejected'
+	| 'post/review/requested'
 
-export type PostsActivityFields = {
+export type PostsActivityActionFields = {
 	postId: string
-	type: PostsActivityType
-	message: string
+	type: 'action'
+	message: PostsActivityActionMessage
 	args: string
 }
+
+export type PostsActivityMessageFields = {
+	postId: string
+	type: 'message'
+	message: string
+	args: null
+}
+
+export type PostsActivityFields =
+	| PostsActivityActionFields
+	| PostsActivityMessageFields
