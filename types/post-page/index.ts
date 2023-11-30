e38f2e5bd097a77d8b3e1588a6e37d11b8316efe
Eduard Aksamitov <e@euaaaio.ru>
2023-11-30 22:18:47 +0300

    chore: remove unnecessary

--- types/post-page/index.ts
+++
@@ -1,7 +0,0 @@
-import type { PageFields } from '../page/index.js'
-
-export type PostPageFields = Omit<PageFields, 'projectId'> & {
-	postId: string
-}
-
-export type PostPage = { id: string } & PostPageFields
