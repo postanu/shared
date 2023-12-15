d6c28d099cced25cabdd38b1937a0bc322059c06
Eduard Aksamitov <e@euaaaio.ru>
2023-12-16 02:24:46 +0300

    feat(attachment): add `state`

--- types/attachment/index.ts
+++ types/attachment/index.ts
@@ -2,11 +2,17 @@ export enum AttachmentType {
 	image = 100
 }
 
+export enum AttachmentState {
+	uploading = 100,
+	uploaded = 200
+}
+
 export type AttachmentFields = {
 	projectId: string
 	type: AttachmentType
 	filename: string
 	src: string
+	state: AttachmentState
 }
 
 export type Attachment = { id: string } & AttachmentFields
